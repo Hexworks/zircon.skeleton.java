bd403e17593192604f338c064a534b93585bd123
Adam Arold <arold.adam@gmail.com>
2020-02-22 00:02:37 +0100

    bump version

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -15,22 +15,33 @@ public class Main {
 
     public static void main(String[] args) {
 
+        // a TileGrid represents a 2D grid composed of Tiles
         TileGrid tileGrid = SwingApplications.startTileGrid(
                 AppConfig.newBuilder()
+                        // The number of tiles horizontally, and vertically
                         .withSize(Size.create(60, 30))
+                        // You can choose from a wide array of CP437, True Type or Graphical tilesets
+                        // which are built into Zircon
                         .withDefaultTileset(CP437TilesetResources.rexPaint16x16())
                         .build());
 
+        // A Screen is an abstraction which lets you use text GUI Components
+        // You can have multiple Screens attached to the same TileGrid to be able to create multiple
+        // screens for your app.
         Screen screen = Screen.create(tileGrid);
 
+        // Creating text GUI Components is super simple
         Label label = Components.label()
                 .withText("Hello, Zircon!")
                 .withPosition(Position.create(23, 10))
                 .build();
 
+        // Screens can hold GUI components
         screen.addComponent(label);
 
         screen.display();
+
+        // Zircon comes with a plethora of built-in color themes
         screen.setTheme(ColorThemes.arc());
     }
 }
